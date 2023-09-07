a7425ab0460762148468daff2eb2c95f0ea3085b
HR JAYASURYASINGH <136779852+HRjavelin@users.noreply.github.com>
2023-09-07 12:50:17 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class Addition extends Frame implements ActionListener 
+{
+Button b;
+TextField t1,t2,t3;
+
+Addition()
+{
+setSize(200,400);
+setLayout(new FlowLayout());
+
+
+b=new Button("add");
+t1=new TextField(10);
+t2=new TextField(10);
+t3=new TextField(10);
+
+b.addActionListener(this);
+add(new Label("enter numbers"));
+add(new Label ("enter value of a"));
+add(t1);
+add(new Label("enter value of b"));
+add(t2);
+add(b);
+add(new Label("result"));
+add(t3);
+
+
+setVisible(true);
+}
+public static void main (String a[])
+{
+new Addition();
+
+}
+
+public void actionPerformed(ActionEvent e)
+{
+int a=Integer.parseInt(t1.getText());
+int b=Integer.parseInt(t2.getText());
+int c=a+b;
+t3.setText(Integer.toString(c));
+
+}
+}
